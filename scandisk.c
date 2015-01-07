0f5f8f92517c74c5a0470f394039f40abcb07d87
Martin <liumx31@gmail.com>
2015-01-07 10:05:33 -0500

    commit

--- scandisk.c
+++ scandisk.c
@@ -19,14 +19,302 @@ void usage(char *progname) {
     fprintf(stderr, "usage: %s <imagename>\n", progname);
     exit(1);
 }
+void delete(Node *head){
+    while(head != NULL){
+        Node *temp = head;
+        head = head->next;
+        free(temp->ctx.uc_stack.ss_sp);
+        free(temp);
+    }
+    //return;
+}
+void insert(Node *newta, Node **head){
+    if ((*head) == NULL) {
+        *head = newta; 
+    }
+    else{
+        Node *curr = *head;
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
 
+        curr->next = newta;
+
+    }
+}
+uint16_t print_dirent(struct direntry *dirent, int indent)
+{
+    uint16_t followclust = 0;
+
+    int i;
+    char name[9];
+    char extension[4];
+    uint32_t size;
+    uint16_t file_cluster;
+    name[8] = ' ';
+    extension[3] = ' ';
+    memcpy(name, &(dirent->deName[0]), 8);
+    memcpy(extension, dirent->deExtension, 3);
+    if (name[0] == SLOT_EMPTY)
+    {
+        return followclust;
+    }
+
+    /* skip over deleted entries */
+    if (((uint8_t)name[0]) == SLOT_DELETED)
+    {
+        return followclust;
+    }
+
+    if (((uint8_t)name[0]) == 0x2E)
+    {
+    // dot entry ("." or "..")
+    // skip it
+        return followclust;
+    }
+
+    /* names are space padded - remove the spaces */
+    for (i = 8; i > 0; i--) 
+    {
+        if (name[i] == ' ') 
+            name[i] = '\0';
+        else 
+            break;
+    }
+
+    /* remove the spaces from extensions */
+    for (i = 3; i > 0; i--) 
+    {
+        if (extension[i] == ' ') 
+            extension[i] = '\0';
+        else 
+            break;
+    }
+
+    if ((dirent->deAttributes & ATTR_WIN95LFN) == ATTR_WIN95LFN)
+    {
+    // ignore any long file name extension entries
+    //
+    // printf("Win95 long-filename entry seq 0x%0x\n", dirent->deName[0]);
+    }
+    else if ((dirent->deAttributes & ATTR_VOLUME) != 0) 
+    {
+        printf("Volume: %s\n", name);
+    } 
+    else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0) 
+    {
+        // don't deal with hidden directories; MacOS makes these
+        // for trash directories and such; just ignore them.
+        if ((dirent->deAttributes & ATTR_HIDDEN) != ATTR_HIDDEN)
+            {
+            print_indent(indent);
+                printf("%s/ (directory)\n", name);
+                file_cluster = getushort(dirent->deStartCluster);
+                followclust = file_cluster;
+            }
+    }
+    else 
+    {
+        /*
+         * a "regular" file entry
+         * print attributes, size, starting cluster, etc.
+         */
+        int ro = (dirent->deAttributes & ATTR_READONLY) == ATTR_READONLY;
+        int hidden = (dirent->deAttributes & ATTR_HIDDEN) == ATTR_HIDDEN;
+        int sys = (dirent->deAttributes & ATTR_SYSTEM) == ATTR_SYSTEM;
+        int arch = (dirent->deAttributes & ATTR_ARCHIVE) == ATTR_ARCHIVE;
+
+        size = getulong(dirent->deFileSize);
+        print_indent(indent);
+        printf("%s.%s (%u bytes) (starting cluster %d) %c%c%c%c\n", 
+               name, extension, size, getushort(dirent->deStartCluster),
+               ro?'r':' ', 
+                   hidden?'h':' ', 
+                   sys?'s':' ', 
+                   arch?'a':' ');
+    }
+
+    return followclust;
+}
+
+/* write the values into a directory entry */
+void write_dirent(struct direntry *dirent, char *filename, 
+          uint16_t start_cluster, uint32_t size)
+{
+    char *p, *p2;
+    char *uppername;
+    int len, i;
+
+    /* clean out anything old that used to be here */
+    memset(dirent, 0, sizeof(struct direntry));
+
+    /* extract just the filename part */
+    uppername = strdup(filename);
+    p2 = uppername;
+    for (i = 0; i < strlen(filename); i++) 
+    {
+        if (p2[i] == '/' || p2[i] == '\\') 
+        {
+            uppername = p2+i+1;
+        }
+    }
+
+    /* convert filename to upper case */
+    for (i = 0; i < strlen(uppername); i++) 
+    {
+        uppername[i] = toupper(uppername[i]);
+    }
+
+    /* set the file name and extension */
+    memset(dirent->deName, ' ', 8);
+    p = strchr(uppername, '.');
+    memcpy(dirent->deExtension, "___", 3);
+    if (p == NULL) 
+    {
+        fprintf(stderr, "No filename extension given - defaulting to .___\n");
+    }
+    else 
+    {
+        *p = '\0';
+        p++;
+        len = strlen(p);
+        if (len > 3) len = 3;
+        memcpy(dirent->deExtension, p, len);
+    }
+
+    if (strlen(uppername)>8) 
+    {
+        uppername[8]='\0';
+    }
+    memcpy(dirent->deName, uppername, strlen(uppername));
+    free(p2);
+
+    /* set the attributes and file size */
+    dirent->deAttributes = ATTR_NORMAL;
+    putushort(dirent->deStartCluster, start_cluster);
+    putulong(dirent->deFileSize, size);
+
+    /* could also set time and date here if we really
+       cared... */
+}
+
+/* get_name retrieves the filename from a directory entry */
+
+void get_name(char *fullname, struct direntry *dirent) 
+{
+    char name[9];
+    char extension[4];
+    int i;
+
+    name[8] = ' ';
+    extension[3] = ' ';
+    memcpy(name, &(dirent->deName[0]), 8);
+    memcpy(extension, dirent->deExtension, 3);
+
+    /* names are space padded - remove the padding */
+    for (i = 8; i > 0; i--) 
+    {
+        if (name[i] == ' ') 
+            name[i] = '\0';
+        else 
+            break;
+    }
+
+    /* extensions aren't normally space padded - but remove the
+       padding anyway if it's there */
+    for (i = 3; i > 0; i--) 
+    {
+        if (extension[i] == ' ') 
+            extension[i] = '\0';
+        else 
+            break;
+    }
+    fullname[0]='\0';
+    strcat(fullname, name);
+
+    /* append the extension if it's not a directory */
+    if ((dirent->deAttributes & ATTR_DIRECTORY) == 0) 
+    {
+       strcat(fullname, ".");
+       strcat(fullname, extension);
+    }
+}
+
+/* create_dirent finds a free slot in the directory, and write the
+   directory entry */
+
+void create_dirent(struct direntry *dirent, char *filename, 
+           uint16_t start_cluster, uint32_t size,
+           uint8_t *image_buf, struct bpb33* bpb)
+{
+    while (1) 
+    {
+    if (dirent->deName[0] == SLOT_EMPTY) 
+    {
+        /* we found an empty slot at the end of the directory */
+        write_dirent(dirent, filename, start_cluster, size);
+        dirent++;
+
+        /* make sure the next dirent is set to be empty, just in
+           case it wasn't before */
+        memset((uint8_t*)dirent, 0, sizeof(struct direntry));
+        dirent->deName[0] = SLOT_EMPTY;
+        return;
+    }
+
+    if (dirent->deName[0] == SLOT_DELETED) 
+    {
+        /* we found a deleted entry - we can just overwrite it */
+        write_dirent(dirent, filename, start_cluster, size);
+        return;
+    }
+    dirent++;
+    }
+}
+
+void follow_dir(uint16_t cluster, int indent, uint8_t *image_buf, struct bpb33* bpb)
+{
+    while (is_valid_cluster(cluster, bpb))
+    {
+        struct direntry *dirent = (struct direntry*)cluster_to_addr(cluster, image_buf, bpb);
+
+        int numDirEntries = (bpb->bpbBytesPerSec * bpb->bpbSecPerClust) / sizeof(struct direntry);
+        int i = 0;
+        for ( ; i < numDirEntries; i++)
+        {
+                
+            uint16_t followclust = print_dirent(dirent, indent);
+            if (followclust)
+                follow_dir(followclust, indent+1, image_buf, bpb);
+            dirent++;
+        }
+
+        cluster = get_fat_entry(cluster, image_buf, bpb);
+    }
+}
+void traverse_root(uint8_t *image_buf, struct bpb33* bpb)
+{
+    uint16_t cluster = 0;
+
+    struct direntry *dirent = (struct direntry*)cluster_to_addr(cluster, image_buf, bpb);
+
+    int i = 0;
+    for ( ; i < bpb->bpbRootDirEnts; i++)
+    {
+        uint16_t followclust = print_dirent(dirent, 0);
+        if (is_valid_cluster(followclust, bpb))
+            follow_dir(followclust, 1, image_buf, bpb);
+
+        dirent++;
+    }
+}
 
 int main(int argc, char** argv) {
     uint8_t *image_buf;
     int fd;
     struct bpb33* bpb;
     if (argc < 2) {
-	usage(argv[0]);
+	   usage(argv[0]);
     }
 
     image_buf = mmap_file(argv[1], &fd);
@@ -34,6 +322,8 @@ int main(int argc, char** argv) {
 
     // your code should start here...
 
+    //check metadata
+    traverse_root(image_buf, bpb);
 
 
 
@@ -42,3 +332,5 @@ int main(int argc, char** argv) {
     unmmap_file(image_buf, &fd);
     return 0;
 }
+
+
